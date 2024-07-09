3e73b8081c7626a894397d68c8c85d0585b52c29
mmccjjj <dhuber88@sunrise.ch>
2024-07-09 16:58:05 +0200

    übung1

---
+++ authors.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `library`
+ */
+
+/* `library`.`authors` */
+
+// Ist etwas unleserlich dargestellt aber eine nützliche funktion..
+$authors = array(
+  array(  
+          'id' => '1',
+          'gender' => 'm',
+          'firstname' => 'Max',
+          'lastname' => 'Muster',
+          'birthdate' => '1955-06-06',
+          'died_on' => NULL,
+          'amount_of_children' => '0',
+          'has_siblings' => '1',
+          'nationality' => 'swiss',
+          'wikipedia_links' => NULL,
+          'deleted_at' => '2024-07-09'
+        ),
+  array('id' => '5','gender' => 'f','firstname' => 'J.K.','lastname' => 'Rowling','birthdate' => '1965-07-31','died_on' => NULL,'amount_of_children' => '3','has_siblings' => '1','nationality' => 'British','wikipedia_links' => 'https://en.wikipedia.org/wiki/J._K._Rowling','deleted_at' => NULL),
+  array('id' => '7','gender' => 'm','firstname' => 'J.R.R.','lastname' => 'Tolkien','birthdate' => '1892-01-03','died_on' => '1973-09-02','amount_of_children' => '4','has_siblings' => '1','nationality' => 'British','wikipedia_links' => 'https://en.wikipedia.org/wiki/J._R._R._Tolkien','deleted_at' => NULL),
+  array('id' => '9','gender' => 'f','firstname' => 'Jodi','lastname' => 'Picoult','birthdate' => '1966-05-19','died_on' => NULL,'amount_of_children' => '3','has_siblings' => '1','nationality' => 'American','wikipedia_links' => 'https://en.wikipedia.org/wiki/Jodi_Picoult','deleted_at' => NULL),
+  array('id' => '10','gender' => 'm','firstname' => 'George R.R.','lastname' => 'Martin','birthdate' => '1948-09-20','died_on' => NULL,'amount_of_children' => '3','has_siblings' => '1','nationality' => 'American','wikipedia_links' => 'https://en.wikipedia.org/wiki/George_R._R._Martin','deleted_at' => NULL),
+  array('id' => '11','gender' => 'f','firstname' => 'Jojo','lastname' => 'Moyes','birthdate' => '1969-08-04','died_on' => NULL,'amount_of_children' => '3','has_siblings' => '1','nationality' => 'British','wikipedia_links' => 'https://en.wikipedia.org/wiki/Jojo_Moyes','deleted_at' => NULL),
+  array('id' => '12','gender' => 'm','firstname' => 'Paulo','lastname' => 'Coelho','birthdate' => '1947-08-24','died_on' => NULL,'amount_of_children' => '1','has_siblings' => '0','nationality' => 'Brazilian','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '13','gender' => 'f','firstname' => 'Suzanne','lastname' => 'Collins','birthdate' => '1962-08-10','died_on' => NULL,'amount_of_children' => '2','has_siblings' => '1','nationality' => 'American','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '14','gender' => 'm','firstname' => 'Ernest','lastname' => 'Hemingway','birthdate' => '1899-07-21','died_on' => '1961-07-02','amount_of_children' => '3','has_siblings' => '1','nationality' => 'American','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '15','gender' => 'f','firstname' => 'Virginia','lastname' => 'Woolf','birthdate' => '1882-01-25','died_on' => '1941-03-28','amount_of_children' => '0','has_siblings' => '1','nationality' => 'British','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '16','gender' => 'm','firstname' => 'Arthur','lastname' => 'Conan Doyle','birthdate' => '1859-05-22','died_on' => '1930-07-07','amount_of_children' => '2','has_siblings' => '1','nationality' => 'British','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '17','gender' => 'm','firstname' => 'F. Scott','lastname' => 'Fitzgerald','birthdate' => '1896-09-24','died_on' => '1940-12-21','amount_of_children' => '1','has_siblings' => '1','nationality' => 'American','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '18','gender' => 'm','firstname' => 'Thomas','lastname' => 'Mann','birthdate' => '1875-06-06','died_on' => '1955-08-12','amount_of_children' => '6','has_siblings' => '1','nationality' => 'German','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '19','gender' => 'f','firstname' => 'Herta','lastname' => 'Müller','birthdate' => '1953-08-17','died_on' => NULL,'amount_of_children' => '1','has_siblings' => '0','nationality' => 'German','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '20','gender' => 'm','firstname' => 'Hermann','lastname' => 'Hesse','birthdate' => '1877-07-02','died_on' => '1962-08-09','amount_of_children' => '3','has_siblings' => '1','nationality' => 'German','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '21','gender' => 'f','firstname' => 'Annette','lastname' => 'von Droste-Hülshoff','birthdate' => '1797-01-10','died_on' => '1848-05-24','amount_of_children' => '0','has_siblings' => '1','nationality' => 'German','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '22','gender' => 'm','firstname' => 'Günter','lastname' => 'Grass','birthdate' => '1927-10-16','died_on' => '2015-04-13','amount_of_children' => '4','has_siblings' => '1','nationality' => 'German','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '23','gender' => 'f','firstname' => 'Joël','lastname' => 'Dicker','birthdate' => '1985-06-16','died_on' => NULL,'amount_of_children' => '2','has_siblings' => '1','nationality' => 'swiss','wikipedia_links' => NULL,'deleted_at' => '2024-07-09'),
+  array('id' => '24','gender' => 'm','firstname' => 'Max','lastname' => 'Frisch','birthdate' => '1911-05-15','died_on' => '1991-04-04','amount_of_children' => '2','has_siblings' => '1','nationality' => 'swiss','wikipedia_links' => NULL,'deleted_at' => '2024-07-09'),
+  array('id' => '25','gender' => 'f','firstname' => 'Ella','lastname' => 'Moser','birthdate' => '1926-03-28','died_on' => NULL,'amount_of_children' => '3','has_siblings' => '1','nationality' => 'swiss','wikipedia_links' => NULL,'deleted_at' => '2024-07-09'),
+  array('id' => '26','gender' => 'm','firstname' => 'Friedrich','lastname' => 'Dürrenmatt','birthdate' => '1921-01-05','died_on' => '1990-12-14','amount_of_children' => '3','has_siblings' => '1','nationality' => 'swiss','wikipedia_links' => NULL,'deleted_at' => '2024-07-09'),
+  array('id' => '27','gender' => 'f','firstname' => 'Alice','lastname' => 'Rohrwacher','birthdate' => '1982-01-29','died_on' => NULL,'amount_of_children' => '1','has_siblings' => '1','nationality' => 'swiss','wikipedia_links' => NULL,'deleted_at' => '2024-07-09'),
+  array('id' => '29','gender' => 'f','firstname' => 'Isabel','lastname' => 'Allende','birthdate' => '1942-08-02','died_on' => NULL,'amount_of_children' => '2','has_siblings' => '1','nationality' => 'Chilean','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '30','gender' => 'm','firstname' => 'Gabriel','lastname' => 'García Márquez','birthdate' => '1927-03-06','died_on' => '2014-04-17','amount_of_children' => '2','has_siblings' => '1','nationality' => 'Colombian','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '31','gender' => 'f','firstname' => 'Simone','lastname' => 'de Beauvoir','birthdate' => '1908-01-09','died_on' => '1986-04-14','amount_of_children' => '0','has_siblings' => '1','nationality' => 'French','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '32','gender' => 'm','firstname' => 'Victor','lastname' => 'Hugo','birthdate' => '1802-02-26','died_on' => '1885-05-22','amount_of_children' => '5','has_siblings' => '1','nationality' => 'French','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '33','gender' => 'f','firstname' => 'Colette','lastname' => '','birthdate' => '1873-01-28','died_on' => '1954-08-03','amount_of_children' => '1','has_siblings' => '1','nationality' => 'French','wikipedia_links' => NULL,'deleted_at' => NULL),
+  array('id' => '34','gender' => 'm','firstname' => 'Miguel','lastname' => 'Cervantes','birthdate' => '1547-09-29','died_on' => '1616-04-23','amount_of_children' => '3','has_siblings' => '1','nationality' => 'Spanish','wikipedia_links' => NULL,'deleted_at' => NULL)
+);
